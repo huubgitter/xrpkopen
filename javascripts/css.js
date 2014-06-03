f3ab246b5ef12c2e0116b19695f4f74394239230
huubgitter <huubgitter@gmail.com>
2014-06-03 20:10:17 +0200

    Create css.js

---
+++ javascripts/css.js
@@ -0,0 +1,9 @@
+      $( document ).ready(function() {
+      $('#ripplewise').click(function(){ 
+        window.open("https://www.ripplewise.com/buy/9c8d2dfc-68b3-4027-a2bf-daee5c351979/");
+      });
+      $('#bitstamp').click(function(){ 
+        window.open("https://www.bitstamp.net/");
+      });
+      });
+    
